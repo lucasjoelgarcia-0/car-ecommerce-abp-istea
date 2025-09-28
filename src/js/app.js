2f5d16974868a6a6e87a810feb71ffc4b305d96e
Lucas Joel <lucasjoelgarcia.0@gmail.com>
2025-09-28 16:51:03 -0300

    Se agrega modal con descripción y formulario de contacto.

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,6 @@
 const menu = document.querySelector('.side-menu');
+const vehicleCards = document.querySelectorAll('.vehicle-card');
+const vehicleDetailModal = document.querySelector('section.vehicle-details .vehicle-detail-modal');
 
 function closeSideMenu() {
     console.log('Se va a cerrar el menú lateral');
@@ -9,3 +11,22 @@ function openSideMenu() {
     console.log('Se va a abrir el menú lateral');
     menu.style.display = 'block';
 }
+
+function closeVehicleDetailsModal() {
+    vehicleDetailModal.style.display = 'none';
+    vehicleDetailModal.classList.remove('show');
+    document.documentElement.style.overflowY = 'auto';
+}
+
+
+vehicleCards.forEach(card => {
+    card.addEventListener('click', (e) => {
+        console.log('clicked', card);
+
+        if (!vehicleDetailModal.classList.contains('show')) {
+            vehicleDetailModal.classList.add('show');
+            vehicleDetailModal.style.display = 'block';
+            document.documentElement.style.overflowY = 'hidden';
+        }
+    });
+})
