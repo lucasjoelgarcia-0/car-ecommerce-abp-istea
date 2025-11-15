8888bec3c176f114776f57619321c3010e2a0071
Lucas Joel Garcia <lucasjoelgarcia.0@gmail.com>
2025-11-14 21:47:52 -0300

    Modifico el buscador para usar el método "every" y que la búsqueda sea más precisa.

--- src/js/services/search-service.js
+++ src/js/services/search-service.js
@@ -1,5 +1,5 @@
 export function searchVehicleService(vehicles = [], searchTerms) {
     return vehicles.filter(vehicle => {
-        return searchTerms.some(term => vehicle.brand.toLowerCase().includes(term.toLowerCase()) || vehicle.model.toLowerCase().includes(term.toLowerCase()));
+        return searchTerms.every(term => vehicle.brand.toLowerCase().includes(term.toLowerCase()) || vehicle.model.toLowerCase().includes(term.toLowerCase()));
     })
 }
