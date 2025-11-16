3c83c26090d0bf743227fd693dce490e7d33ac36
Lucas Joel Garcia <lucasjoelgarcia.0@gmail.com>
2025-11-16 13:33:58 -0300

    Se agregan variables de Airtable para hacer deploy en Gihub pages

---
+++ src/js/services/airtable-envs.js
@@ -0,0 +1,3 @@
+export const AIRTABLE_API_URL = "https://api.airtable.com/v0/";
+export const AIRTABLE_API_TOKEN = "pat88rEIvgykLRUzk.36dec9f80cf5f7109a0e7dcfb6d86d459b41752d53f9643376bea48eb8a988cc";
+export const AIRTABLE_BASE_ID = "appNsF8XieEoqlCsA";
